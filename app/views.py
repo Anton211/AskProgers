f30e9e8b0967d18919d1488242a5aea30a63751b
Anton <mranton21@bk.ru>
2025-04-02 13:10:31 +0300

    Доделал 2ую часть

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 
 
 TAGS = ['Python', 'Django', 'Flask', 'FastApi', 'C++']
 PHOTOS = ['man', 'woman']
+
 QUESTIONS = [
     {'id': i,
      'username': f'User{i}',
@@ -12,7 +14,7 @@ QUESTIONS = [
      'date': '16 Мая 2025 16:45',
      'tags': [TAGS[i % len(TAGS)], TAGS[(i + 1) % len(TAGS)], TAGS[(i + 2) % len(TAGS)]],
      'rate': (-1) ** (i % 2) * i * 10,
-     'num_ans': i
+     'num_ans': i - 1
      } for i in range(1, 31)
 ]
 
@@ -27,13 +29,50 @@ ANSWERS = [
     ] for j in range(len(QUESTIONS))
 ]
 
+def paginate(objects_list, request, per_page):
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(objects_list, per_page)
+    try:
+        page = paginator.page(page_num)
+    except PageNotAnInteger:
+        page = paginator.page(1)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    return page
+
 def index(request):
-    return render(request, 'index.html', context={'questions': QUESTIONS})
+    questions_sort = sorted(QUESTIONS, key=lambda x: x['date'])
+    page = paginate(questions_sort, request, 5)
+    return render(request, 'index.html', context={'questions': page.object_list, 'page': page})
 
 def popular(request):
-    questions_sort = sorted(QUESTIONS, key=lambda x: -x['rate'])
-    return render(request, 'popular.html', context={'questions': questions_sort})
+    questions_sort = sorted(QUESTIONS, key=lambda x: (-x['rate'], x['date']))
+    page = paginate(questions_sort, request, 5)
+    return render(request, 'popular.html', context={'questions': page.object_list, 'page': page})
 
 def question(request, question_id):
+    answers_sort = sorted(ANSWERS[question_id - 1], key=lambda x: (-x['rate'], x['date']))
+    page = paginate(answers_sort, request, 5)
     return render(request, 'question.html',
-                context={'question': QUESTIONS[question_id - 1], 'answers': ANSWERS[question_id - 1]})
+                context={'question': QUESTIONS[question_id - 1], 'answers': page.object_list, 'page': page})
+
+def tag_questions(request, tag):
+    if tag not in TAGS:
+        return index(request)
+    else:
+        questions = [q for q in QUESTIONS if tag in q.get('tags', '')]
+        questions_sort = sorted(questions, key=lambda x: (-x['rate'], x['date']))
+        page = paginate(questions_sort, request, 5)
+        return render(request, 'tag.html', context={'questions': page.object_list, 'tag': tag, 'page': page})
+
+def create_question(request):
+    return render(request, 'ask.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def settings(request):
+    return render(request, 'settings.html')
--- askprogers/urls.py
+++ askprogers/urls.py
@@ -23,6 +23,11 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('popular/', views.popular, name='popular'),
-    path('question/<int:question_id>', views.question, name='question'),
+    path('hot/', views.popular, name='popular'),
+    path('question/<int:question_id>/', views.question, name='question'),
+    path('tag/<str:tag>/', views.tag_questions, name='tag'),
+    path('ask/', views.create_question, name='ask'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('settings', views.settings, name='settings'),
 ]
